0ee034ba8927d2278b8fa5df9ba4b8e062b8ac42
ritatrii04 <ritatrii04@student.uns.ac.id>
2026-09-20 13:47:48 +0700

    Enable Tailwind CSS Vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [react()],
-  test : {
+  plugins: [react(), tailwindcss()],
+  test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/setupTests.ts',
